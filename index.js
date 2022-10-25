a631bc3a26f9d066f6b0b94ea6422874d3ff94d9
farhanacsebd <farhana.cse.bd@gmail.com>
2022-10-26 00:54:05 +0600

    added messages

--- index.js
+++ index.js
@@ -9,4 +9,27 @@ function bmi(){
     const bmi = weight/(height/100 * height/100);
     const total = bmi.toFixed(2);
     result.innerText ="Your BMI is " + total;
+    
+    let report = showMessage(bmi);
+    message.textContent = report;
+}
+
+
+function showMessage(result){
+
+    if(result<16){
+        return "You are Thin"
+    }
+    else if(result >=16 && result <=17){
+        return "You are moderate thin"
+    }
+    else if(result >17 && result <=18.5){
+        return "You are mid thin"
+    }
+    else if(result > 18.5 && result <=25){
+        return "You are normal"
+    }
+    else if(result > 25){
+        return "You are overweight"
+    }
 }
